5341f1a313030e2b58c7b1540cf6a96fbbacb221
HOME_PC\JoshuaMayberry <joshua.mayberry1991@gmail.com>
2024-01-01 10:45:22 -0700

    Added a flag for when overriding is happening

--- Runtime/Scripts/EventManagerBase.cs
+++ Runtime/Scripts/EventManagerBase.cs
@@ -6,30 +6,33 @@ using jmayberry.CustomAttributes;
 
 namespace jmayberry.EventSequencer {
 	public class EventManagerBase : MonoBehaviour {
-        private SequenceBase currentSequence;
-        private Coroutine currentCoroutine;
+		protected SequenceBase currentSequence;
+		protected Coroutine currentCoroutine;
 
-		[Readonly] [SerializeField] private bool isSequenceRunning;
+		[Readonly] [SerializeField] protected bool isOverriding;
+		[Readonly] [SerializeField] protected bool isSequenceRunning;
 
-        public override string ToString() {
+		public override string ToString() {
 			return $"<EventManagerBase:{this.GetHashCode()}>";
 		}
 
-		public void StartSequence(IContext context, SequenceBase sequence, bool hardStop = false) {
+		public virtual void StartSequence(IContext context, SequenceBase sequence, bool hardStop = false) {
 			if (this.isSequenceRunning) {
 				if (!sequence.ShouldOverride(this.currentSequence)) {
 					return;
 				}
 
+				this.isOverriding = true;
 				this.StopSequence(this.currentSequence, this.currentCoroutine, hardStop);
 			}
 
 			this.isSequenceRunning = true;
-            this.currentSequence = sequence;
+			this.currentSequence = sequence;
+			this.isOverriding = false;
 			this.currentCoroutine = StartCoroutine(this.currentSequence.Start(context, OnSequenceFinished));
 		}
 
-		private void OnSequenceFinished(SequenceBase sequence) {
+		protected virtual void OnSequenceFinished(SequenceBase sequence) {
 			if (sequence == this.currentSequence) {
 				this.isSequenceRunning = false;
 			}
@@ -39,12 +42,12 @@ namespace jmayberry.EventSequencer {
 			StopSequence(this.currentSequence, this.currentCoroutine, hardStop);
 		}
 
-		public void StopSequence(SequenceBase sequence, Coroutine coroutine, bool hardStop = false) {
+		public virtual void StopSequence(SequenceBase sequence, Coroutine coroutine, bool hardStop = false) {
 			StartCoroutine(this.Cancel(sequence, coroutine, hardStop));
 			this.isSequenceRunning = false;
 		}
 
-		private IEnumerator Cancel(SequenceBase sequence, Coroutine coroutine, bool hardStop = false) {
+		protected virtual IEnumerator Cancel(SequenceBase sequence, Coroutine coroutine, bool hardStop = false) {
 			if (coroutine == null) {
 				yield break;
 			}
